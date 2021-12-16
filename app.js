580346b73814bf202fa3466e30f950140d29ba34
Kyra Christensen <christensen.kyral@gmail.com>
2021-12-16 09:48:52 -0800

    started working on it

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 // import functions and grab DOM elements
-
+const signUpForm = document.querySelector('#sign-up');
 // let state
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+signUpForm.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+    const user = await signUpUser
+})
---
+++ fetch.utils.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = "https://ritiyenwzsalzpktroey.supabase.co"
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzOTUwODg4NCwiZXhwIjoxOTU1MDg0ODg0fQ.Yn9Ken8agdL7K8NpTPyu81cBonK6zQDKTwIrFdY-xwM'
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
